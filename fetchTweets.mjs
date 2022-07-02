bad550750c19c344321e50c834a5bad2c4005902
Julian Toscani <julian_toscani@gmx.de>
2022-07-02 13:53:36 +0200

    fix script

--- fetchTweets.mjs
+++ fetchTweets.mjs
@@ -1,17 +1,16 @@
-import fs from "fs/promises";
-import path from "path";
+const headers = {
+  Authorization: `Bearer <token>`,
+};
 
-getTweets()
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+async function fetchTweets(name) {
+  const idResponse = await fetch(
+    `https://api.twitter.com/2/users/by/username/${name}`,
+    { headers }
+  );
+  const {
+    data: { id },
+  } = await idResponse.json();
 
-async function getTweets() {
-  const headers = await getReqeustHeaders();
-  const id = getTwitterId(headers);
-  return await fetchTweets(id);
-}
-
-async function fetchTweets(id) {
   const response = await fetch(`https://api.twitter.com/2/users/${id}/tweets`, {
     headers,
   });
@@ -21,28 +20,6 @@ async function fetchTweets(id) {
   return await response.json();
 }
 
-async function getTwitterId(headers, username = "PlayStormgate") {
-  const idResponse = await fetch(
-    `https://api.twitter.com/2/users/by/username/${username}`,
-    { headers }
-  );
-  const {
-    data: { id },
-  } = await idResponse.json();
-
-  return id;
-}
-
-async function getReqeustHeaders() {
-  const token = await fs.readFile(
-    path.resolve(__dirname, "twitter_token.txt"),
-    {
-      encoding: "utf8",
-    }
-  );
-  const headers = {
-    Authorization: `Bearer ${token}`,
-  };
-
-  return headers;
-}
+fetchTweets("PlayStormgate")
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
