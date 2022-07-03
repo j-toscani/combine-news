9ed070e2206e46246a54296ec05e6d4e1bbbf277
Julian Toscani <julian.toscani@rtl.com>
2022-07-03 10:42:53 +0200

    implement components, add title

--- app.vue
+++ app.vue
@@ -1,20 +1,40 @@
 <template>
-  <div>
-    <h1>
-      Hello World!
-    </h1>
-    <pre>
-      {{data}}
-    </pre>
-  </div>
+  <header>
+    <div class="container">
+      <h1>News on Stormgate!</h1>
+    </div>
+  </header>
+  <main class="container">
+    <NewsContainer>
+      <template #reddit>
+        <RedditPost
+          v-for="(post, index) in data.redditPosts"
+          :post="post"
+          :key="`reddit-${index}`"
+        />
+      </template>
+      <template #twitter>
+        <TwitterPost
+          v-for="(tweet, index) in data.tweets"
+          :tweet="tweet"
+          :key="`twitter-${index}`"
+        />
+      </template>
+    </NewsContainer>
+  </main>
 </template>
 
 <script lang="ts">
-
 export default defineNuxtComponent({
   async setup() {
-    const {data} = await useAsyncData('news', () => $fetch("/api/news"))
-    return {data};
-  }
-})
+    const { data } = await useAsyncData("news", () => $fetch("/api/news"));
+    return { data };
+  },
+});
 </script>
+
+<style scoped>
+main {
+  @apply flex gap-1;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss'],
-    env: {
-        TWITTER_API_TOKEN: process.env.TWITTER_API_TOKEN
-    }
-})
+  head: {
+    titleTemplate: "News on Stormgate",
+  },
+  modules: ["@nuxtjs/tailwindcss"],
+  env: {
+    TWITTER_API_TOKEN: process.env.TWITTER_API_TOKEN,
+  },
+});
