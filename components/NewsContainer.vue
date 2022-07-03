8bcc897336e717934c20449e9bd85fec386e3515
Julian Toscani <julian.toscani@rtl.com>
2022-07-03 10:40:54 +0200

    💄 add newscontainer component

---
+++ components/NewsContainer.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="tabs-container">
+      <h2>Reddit posts</h2>
+      <h2>Tweets</h2>
+    </div>
+    <section>
+      <ul>
+        <slot name="reddit"></slot>
+      </ul>
+
+      <ul>
+        <slot name="twitter"></slot>
+      </ul>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+.tabs-container,
+section {
+    @apply flex gap-4;
+}
+
+h2, ul {
+    @apply w-1/2;
+}
+
+ul {
+    @apply flex flex-col;
+}
+
+ul >>> li {
+    @apply border-b;
+}
+
+h2 {
+    @apply pl-4;
+    @apply text-2xl;
+}
+</style>
