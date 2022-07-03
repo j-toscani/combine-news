4602ec48e967278c9ea9495fbb92133eadce7878
Julian Toscani <julian.toscani@rtl.com>
2022-07-03 10:40:40 +0200

    💄 add post components

---
+++ components/RedditPost.vue
@@ -0,0 +1,47 @@
+<template>
+  <li :class="post.stickied ? 'stickied' : ''">
+    <a :href="post.url">
+      <h3>{{ post.title }}</h3>
+      <p>{{slicedText}}</p>
+    </a>
+  </li>
+</template>
+
+<script lang="ts" setup>
+interface RedditPost {
+  selftext: string;
+  stickied: boolean;
+  title: string;
+  url: string;
+}
+
+const props = defineProps({
+  post: {
+    type: Object as () => RedditPost,
+  },
+});
+
+const slicedText = computed(() =>
+  props.post.selftext.length > 140
+    ? props.post.selftext.slice(0, 140) + "..."
+    : props.post.selftext
+);
+</script>
+
+<style scoped>
+p {
+    @apply break-words;
+}
+
+h3 {
+    @apply mb-2;
+}
+
+p {
+    @apply text-sm;
+}
+
+li {
+    @apply p-4;
+}
+</style>
